d25911302c79007b467c06b4b309adc4c919f2cf
Felpower <felbauer.patrick@gmail.com>
2023-10-03 13:27:08 +0200

    Deploy to GitHub Pages

--- sw.js
+++ sw.js
@@ -1,24 +1,24 @@
-   self.addEventListener('install', (event) => {
-     console.log('[Service Worker] Installing Service Worker...', event);
-   });
+self.addEventListener('install', (event) => {
+ console.log('[Service Worker] Installing Service Worker...', event);
+});
 
-   self.addEventListener('activate', (event) => {
-     console.log('[Service Worker] Activating Service Worker...', event);
-     return self.clients.claim();
-   });
+self.addEventListener('activate', (event) => {
+ console.log('[Service Worker] Activating Service Worker...', event);
+ return self.clients.claim();
+});
 
-   self.addEventListener('message', (event) => {
-   console.log('Scheduling Notification', event);
-     if (event.data.action === 'scheduleNotification') {
-       const delay = event.data.delay;
-       const title = event.data.title;
-       const options = {
-         body: event.data.body,
-         icon: event.data.icon,  // Optional icon path
-       };
+self.addEventListener('message', (event) => {
+console.log('Scheduling Notification', event);
+ if (event.data.action === 'scheduleNotification') {
+   const delay = event.data.delay;
+   const title = event.data.title;
+   const options = {
+     body: event.data.body,
+     icon: event.data.icon,  // Optional icon path
+   };
 
-       setTimeout(() => {
-         self.registration.showNotification(title, options);
-       }, delay);
-     }
-   });
+   setTimeout(() => {
+     self.registration.showNotification(title, options);
+   }, delay);
+ }
+});
