49c0429c856ab51927ac100486f109d13323775a
Felpower <felbauer.patrick@gmail.com>
2023-10-04 17:02:29 +0200

    Deploy to GitHub Pages

--- sw.js
+++ sw.js
@@ -1,22 +1,21 @@
 self.addEventListener('install', (event) => {
-    console.log('[Service Worker] Installing Service Worker...', event);
+    console.info('[Service Worker] Installing Service Worker...', event);
 });
 
 self.addEventListener('activate', (event) => {
-    console.log('[Service Worker] Activating Service Worker...', event);
+    console.info('[Service Worker] Activating Service Worker...', event);
     return self.clients.claim();
 });
 
 self.addEventListener('message', (event) => {
-    console.log('Scheduling Notification', event);
+    console.info('Scheduling Notification', event);
     if (event.data.action === 'scheduleNotification') {
         const delay = event.data.delay;
         const title = event.data.title;
         const options = {
             body: event.data.body,
         };
-        console.log('Successfully scheduled', delay, title, options);
-        self.registration.showNotification(title, options);
+        console.info('Successfully scheduled', delay, title, options);
         setTimeout(() => {
             self.registration.showNotification(title, options);
         }, delay);
