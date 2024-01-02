6959b2319fdba91323bf1b047c4cdd825fb159cb
Felpower <felbauer.patrick@gmail.com>
2024-01-02 21:32:52 +0100

    Deploy to GitHub Pages

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,13 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCcBYFUJbTyRWUjy6dhLbLLEj_lwhqnsh4",
-  authDomain: "darkpatterns-ac762.firebaseapp.com",
-  databaseURL: "https://darkpatterns-ac762-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "darkpatterns-ac762",
-  storageBucket: "darkpatterns-ac762.appspot.com",
-  messagingSenderId: "552263184384",
-  appId: "1:552263184384:web:87e17944dc571dc4e028e5"
+    apiKey: "AIzaSyCcBYFUJbTyRWUjy6dhLbLLEj_lwhqnsh4",
+    authDomain: "darkpatterns-ac762.firebaseapp.com",
+    databaseURL: "https://darkpatterns-ac762-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "darkpatterns-ac762",
+    storageBucket: "darkpatterns-ac762.appspot.com",
+    messagingSenderId: "552263184384",
+    appId: "1:552263184384:web:87e17944dc571dc4e028e5"
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -17,35 +17,22 @@ const messaging = firebase.messaging();
 
 let darkPatternsValue = 0;
 
-self.addEventListener('message', function(event){
-  if (event.data && event.data.type === 'SET_DARK_PATTERNS_VALUE') {
-    darkPatternsValue = event.data.value;
-  }
+self.addEventListener('message', function(event) {
+    if (event.data && event.data.type === 'SET_DARK_PATTERNS_VALUE') {
+        darkPatternsValue = event.data.value;
+    }
 });
 
-messaging.onBackgroundMessage(function (payload) {
+messaging.onBackgroundMessage(function(payload) {
     console.log('Received background message ', payload);
-    if(darkPatternsValue != 1){
-      return;
+    if (darkPatternsValue != 1) {
+        return;
     }
 
-    const notificationTitle = payload.notification.title;
+    const notificationTitle = payload.data.title;
     const notificationOptions = {
-        body: payload.notification.body,
-        actions: [
-            {
-              action: 'yes',
-              type: 'button',
-              title: '👍 Yes',
-            },
-            {
-              action: 'no',
-              type: 'text',
-              title: '👎 No (explain why)',
-              placeholder: 'Type your explanation here',
-            },
-          ],
-      };
+        body: payload.data.body,
+    };
 
     return self.registration.showNotification(notificationTitle, notificationOptions);
 });
