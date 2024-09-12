cc6a1e96841a211723e8797fd97c955095666428
Felpower <felbauer.patrick@gmail.com>
2024-09-12 11:05:20 +0200

    Deploy to GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"062b3a72fcbe76296ce19b02f58c03ba0cbb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "282638022"
+    serviceWorkerVersion: "2342202412"
   }
 });
